47358207473439d3ec9d618c8474bee988edf21a
Dmitry <lmahaon@yandex.ru>
2024-11-12 18:40:27 +0300

    Add debug tool. Add DB queries in ice_cream

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title').filter(Q(is_published__exact=True) & (Q(is_on_main__exact=True) | Q(title__icontains='пломбир')))
+    context = {'ice_cream_list': ice_cream_list, }
+    return render(request, template, context)
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
